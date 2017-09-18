e1bc66473d6f6155345090f7aae7d4d986d0212f
Aldo Adrian Andrew Lopez <aaandrewlopez@gmail.com>
2017-09-18 11:39:42 -0700

    Se eliminaron fuentes que no han sido modificados.

--- Fuentes/Reporte de Movimientos/Logica/Programas.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Programas
-
-    Public Shared prefijoBaseDatosAlmacen As String
-    Public Shared bdCatalogo As String
-    Public Shared bdConfiguracion As String
-    Public Shared bdAlmacen As String
-
-End Class
